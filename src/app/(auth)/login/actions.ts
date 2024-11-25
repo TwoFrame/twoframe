6debd66416ccfd04a92e4023a6da51a11b022ab4
Sean Carlyle <808jalapeno@gmail.com>
2024-11-25 01:08:05 -0800

    adding supa-base signup and login

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -1,20 +1,47 @@
 "use server";
 
 import { LoginSchema } from "@/app/_lib/schemas";
+import { LoginState } from "../types";
 
-export async function login(state: any, formData: FormData) {
-   //1. Form validation, if form not filled correctly display error to user (red text)
-   const validationResult = LoginSchema.safeParse({
-    email: formData.get('email'),
-    password: formData.get('password'),
-  })
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function login(
+  prevState: LoginState | null,
+  formData: FormData
+): Promise<LoginState> {
+
+  const supabase = await createClient()
+
+  // Validate form Data
+  const validationResult = LoginSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 
   if (!validationResult.success) {
     return {
-      errors: validationResult.error.flatten().fieldErrors
-    }
+      errors: validationResult.error.flatten().fieldErrors,
+      success: false
+    };
   }
+
+  // Console Info
+  const { email, password } = validationResult.data;
   console.log("HANDLE LOGIN");
-  console.log("EMAIL:" + formData.get("email"));
-  console.log("USERNAME:" + formData.get("username"));
+  console.log("EMAIL:" + email);
+  console.log("PASSWORD:" + password);
+
+
+  // Attempt to sign in
+  const { error } = await supabase.auth.signInWithPassword({ email, password })
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
 }
--- src/app/(auth)/signup/actions.ts
+++ src/app/(auth)/signup/actions.ts
@@ -1,23 +1,46 @@
 "use server";
 
 import { SignupSchema } from "@/app/_lib/schemas";
+import { SignupState } from "../types";
 
-export async function signup(state: any, formData: FormData) {
-  //1. Form validation, if form not filled correctly display error to user (red text)
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function signup(
+  prevState: SignupState | null,
+  formData: FormData
+): Promise<SignupState> {
+
+  const supabase = await createClient()
+
+  
   const validationResult = SignupSchema.safeParse({
-    username: formData.get('username'),
-    email: formData.get('email'),
-    password: formData.get('password'),
-    cpassword: formData.get('cpassword'),
-  })
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    cpassword: formData.get("cpassword"),
+  });
 
   if (!validationResult.success) {
     return {
       errors: validationResult.error.flatten().fieldErrors
-    }
+    };
   }
-  
-  console.log("TODO: ACTUALLY SIGN UP USER")
 
+  const { email, password } = validationResult.data;
+  console.log("HANDLE SIGNUP");
+  console.log("EMAIL:" + email);
+  console.log("PASSWORD:" + password);
 
+  const { error } = await supabase.auth.signUp({ email, password })
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
 }
