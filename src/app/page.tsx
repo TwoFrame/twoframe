bb1fcce0e7d156aa0abc1e940d5c5f448bf3b992
Sean Carlyle <808jalapeno@gmail.com>
2024-11-29 19:54:41 -0800

    adding logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from '@/app/components/navbar';
+
 export default function Landing() {
   return (
     <div className="hero bg-base-200 min-h-screen">
+      <Navbar />
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">twoframe</h1>
