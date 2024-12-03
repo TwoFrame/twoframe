1ebdc5dbdfc24ddb60bc51ef50bb4c55d58f3080
Sean Carlyle <808jalapeno@gmail.com>
2024-12-03 00:18:31 -0800

    changing db

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,12 +1,11 @@
 import { redirect } from 'next/navigation'
-
 import { createClient } from '@/utils/supabase/server'
 
 export default async function PrivatePage() {
   const supabase = await createClient()
 
   const { data, error } = await supabase.auth.getUser()
-  console.log(data)
+  
   if (error || !data?.user) {
     redirect('/login')
   }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,13 @@
-import { pgTable, varchar, uuid, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const UserTable = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  username: varchar("username", { length: 255 }).notNull().unique(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  password: varchar("password", { length: 255 }).notNull(),
-  created_at: timestamp("created_at").defaultNow().notNull(),
+export const authUsers = pgTable('auth.users', {
+  id: uuid('id').primaryKey(),
+  email: text('email').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+});
+
+export const UserTable = pgTable("usernames", {
+  id: uuid('id').primaryKey().references(() => authUsers.id),
+  username: text('username').notNull().unique(),
+  createdAt: timestamp('created_at').defaultNow(),
 });
