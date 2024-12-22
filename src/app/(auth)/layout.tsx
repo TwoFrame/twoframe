39f7aac14898daba34e57fbef23b16e3b2ca6186
William Kim <wik002@ucsd.edu>
2024-12-21 21:27:32 -0800

    removed old comments from auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,24 +1,4 @@
-// import AuthNavigationBar from "../components/auth-navbar";
-// import "../styles/globals.css";
-
-// export default function BaseLayout({
-//   children, // will be a page or nested layout
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en" data-theme="dark" className="bg-background-default">
-//       <AuthNavigationBar />
-//       <body className="mt-48">
-//         {children}
-//         </body>
-//     </html>
-//   );
-// }
-
-
 import AuthNavigationBar from "../../components/auth-navbar";
-import NavigationBar from "../../components/navbar";
 import "../styles/globals.css";
 
 export default function BaseLayout({
