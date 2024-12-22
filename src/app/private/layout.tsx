5e85b3f3442be4a2a3bc6d19df21c2cdaff0295f
William Kim <wik002@ucsd.edu>
2024-12-21 21:26:56 -0800

    added layout to private page

---
+++ src/app/private/layout.tsx
@@ -0,0 +1,11 @@
+
+export default function PrivateLayout({children}: { children: React.ReactNode; }) {
+
+  return (
+    <html lang="en" data-theme="dark" className="bg-background-default">
+        <body>
+            {children}
+        </body>
+    </html>
+  )
+}
