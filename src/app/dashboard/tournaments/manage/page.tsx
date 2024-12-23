70a36db1743b69ef18b11ca0b88d59e5f5d33730
William Kim <wik002@ucsd.edu>
2024-12-22 23:58:49 -0800

    updated pages to utilize useSidebar() to control mobile sidebar

--- src/app/dashboard/tournaments/manage/page.tsx
+++ src/app/dashboard/tournaments/manage/page.tsx
@@ -1,14 +1,33 @@
+"use client";
+
+import { useSidebar } from "@/components/ui/sidebar";
+import { Button } from "@nextui-org/button";
+import { PanelLeft } from "lucide-react";
 
 export default function ManagePage() {
+
+    const {
+        setOpenMobile,
+    } = useSidebar();
     return (
-        <section className="flex flex-col">
+        <section className="flex flex-col bg-background-default lg:w-3/4 h-screen">
 
             {/* Custom dashboard nav title bar that displays basic info */}
-            <section className="dashboard-nav flex items-center " >
+            <div className="dashboard-nav flex items-center justify-between w-3/4" >
                 <h1 className="text-xl font-semibold">
                     Manage
                 </h1>
-            </section>
+                <Button onPress={() => {
+                    setOpenMobile(true)
+                }}
+                    isIconOnly
+                    variant="light"
+                >
+                    <PanelLeft size={16} />
+                </Button>
+            </div>
         </section>
-    )
+
+
+    );
 }
--- src/app/dashboard/tournaments/search/page.tsx
+++ src/app/dashboard/tournaments/search/page.tsx
@@ -1,16 +1,36 @@
-export default function SearchPage() {
-  return (
-    <section className="flex flex-col">
+"use client";
 
-      {/* Custom dashboard nav title bar that displays basic info */}
-      <section className="dashboard-nav flex items-center " >
-        <h1 className="text-xl font-semibold">
-          Find tournaments
-        </h1>
+import { useSidebar } from "@/components/ui/sidebar";
+import { Button } from "@nextui-org/button";
+import { PanelLeft } from "lucide-react";
+
+export default function SearchPage() {
+
+  const {
+    setOpenMobile,
+  } = useSidebar();
+
+  return (
+      <section className="flex flex-col bg-background-default lg:w-3/4 h-screen">
+
+        {/* Custom dashboard nav title bar that displays basic info */}
+        <div className="dashboard-nav flex items-center justify-between w-3/4" >
+          <h1 className="text-xl font-semibold">
+            Search for tournaments
+          </h1>
+          <Button onPress={()=>{
+            setOpenMobile(true)
+          }}
+          isIconOnly
+          variant="light"
+        >
+          <PanelLeft size={16}/>
+        </Button>
+        </div>
       </section>
-    </section>
     
-    /*TODO: add actual search feature for this page, and display existing tournaments
-               should probably add tournament creation first!*/
+    
   );
+  {/*TODO: add actual search feature for this page, and display existing tournaments
+               should probably add tournament creation first!*/}
 }
