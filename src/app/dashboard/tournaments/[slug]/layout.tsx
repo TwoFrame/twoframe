44160564b280bd61b2dce503b79162adec65ab51
William Kim <wik002@ucsd.edu>
2024-12-27 19:50:41 -0800

    saving project changes

---
+++ src/app/dashboard/tournaments/[slug]/layout.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function SlugLayout({children}: { children: React.ReactNode; }) {
+  
+    return (
+      <html lang="en" data-theme="dark" className="bg-background-default w-full">
+        <body>
+
+            {children}
+        </body>
+      </html>
+  
+    );
+  }
+  
