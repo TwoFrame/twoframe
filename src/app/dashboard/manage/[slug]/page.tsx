3baa00b7df93a51a807f58adbdd540b64f8e50c2
William Kim <wik002@ucsd.edu>
2025-01-01 16:40:34 -0800

    created separate route for managing tournaments

---
+++ src/app/dashboard/manage/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import { getTournamentBySlug } from "@/db/queries/select";
+
+export default async function TournamentPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+
+  const { tournament_data, error } = await getTournamentBySlug(slug);
+
+  if (tournament_data) {
+    return (
+      <div>
+        <h1>Tournament: {tournament_data.title}</h1>
+        <p>
+          Start Date: {new Date(tournament_data.start_date).toLocaleString()}
+        </p>
+        <p>End Date: {new Date(tournament_data.end_date).toLocaleString()}</p>
+      </div>
+    );
+  }
+
+  return <div>Error: {error}</div>;
+}
