23856590b4970a4c95e3216ce4d657b680f40d3d
William Kim <wik002@ucsd.edu>
2025-01-04 22:52:52 -0800

    moved public chip component to folder

---
+++ src/components/public-chip.tsx
@@ -0,0 +1,8 @@
+export default function PublicChip({is_public} : {is_public: boolean}) {
+    return (
+        <>
+        {is_public && <p className="text-xs font-semibold text-color-lime bg-background-light-green py-1 px-2 rounded-md">public</p>}
+        {!is_public && <p className="text-xs font-semibold text-color-light-blue bg-primary py-1 px-2 rounded-md">private</p>}
+        </>
+    )
+}
