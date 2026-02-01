ebd0cf882325462884d4ace816f94f78d8863b23
Sean Carlyle <scarlyle20@gmail.com>
2026-01-31 23:23:47 -0800

    Add files to backend

---
+++ twoframe-server/src/models/tournament.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+# -------------------------
+# Tables
+# -------------------------
+
+class Tournament(BaseModel):
+    tournament_id: str
+    name: str
+    date: str
+    admin_code: str
+    attendee_code: str
+
+
+class Attendee(BaseModel):
+    tournament_id: str
+    attendee_id: str
+    name: str
+
+
+# -------------------------
+# API Payloads
+# -------------------------
+
+class CreateTournamentPayload(BaseModel):
+    name: str
+    date: str
+
+
+class CreateTournamentResponse(BaseModel):
+    tournament_id: str
+    admin_code: str
+    attendee_code: str
+
+
+class JoinTournamentPayload(BaseModel):
+    attendee_code: str
+    name: str
