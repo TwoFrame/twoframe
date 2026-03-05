d3c1accb33623ad2f845b60e5e38d82fa9bc805d
Sean Carlyle <scarlyle20@gmail.com>
2026-03-05 00:16:37 -0800

    Edit config to make key optional

--- twoframe-server/src/config.py
+++ twoframe-server/src/config.py
@@ -1,15 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from typing import Optional
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", env_ignore_empty=True
     )
     aws_region: str
-    aws_access_key: str
-    aws_secret_key: str
+    aws_access_key: Optional[str] = None
+    aws_secret_key: Optional[str] = None
     tournament_table_name: str
     attendee_table_name: str
-
 
 settings = Settings()
